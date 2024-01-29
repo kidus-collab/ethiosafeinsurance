76774034793d64295acf2d72bd41b04e22803e43
kidus-collab <guyfreaky894@gmail.com>
2024-01-29 09:03:48 +0300

    final commit

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 // dom
 //hamburger menu
-//dropdown
 //popup menu
-//onClick picture change
-const quote = getElementById(quote);
 
-quote.onClick();
+const quote_api = "https://api.quotable.io/random";
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const quoteBox = document.getElementById("quote-box");
+
+// quote fetching API
+async function getquote(quote_api) {
+  const response = await fetch(quote_api);
+  var data = await response.json();
+
+  // puts the api data in the html
+  quote.innerHTML = `"<i>${data.content}"</i>`;
+  author.innerHTML = data.author;
+  quoteBox.style.display = "block";
+}
+
+//onClick picture change
